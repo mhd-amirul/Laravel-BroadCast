ebf4c97c6710514fe12c1b9d002525083be64e21
mhd-amirul <amirulm236@gmail.com>
2023-02-12 21:57:29 +0800

    feat: make sign in api

--- app/Http/Controllers/AppAuthController.php
+++ app/Http/Controllers/AppAuthController.php
@@ -2,9 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class AppAuthController extends Controller
 {
+    public function sign_up(Request $request)
+    {
+        $user = $request->all();
+
+        $val = Validator::make($user, [
+            "email" => "required|email|unique:users,email",
+            "name" => "required",
+            "password" => "required|confirmed",
+            "password_confirmation" => "required"
+        ]);
+
+        if ($val->fails()) {
+            return response()->json($val->errors(), 400);
+        }
+
+        $user["password"] = Hash::make($user["password"]);
+
+        User::create($user);
+
+        return response()->json("sign up success!", 200);
+    }
 
 
 }
