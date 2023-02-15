3b5ede0fe1a9aef7afac491ee09061c4576bf45b
mhd-amirul <amirulm236@gmail.com>
2023-02-15 09:35:48 +0700

    fix: refactoring sign_up function

--- app/Services/api_auth.php
+++ app/Services/api_auth.php
@@ -2,9 +2,26 @@
 
 namespace App\Services;
 
+use App\Models\User;
 use App\Services\Interfaces\api_auth as InterfacesApi_auth;
+use Illuminate\Support\Facades\Hash;
 
 class api_auth implements InterfacesApi_auth {
 
+    public function sign_up(array $user)
+    {
+        try {
 
+            $user["password"] = Hash::make($user["password"]);
+
+            $user = User::create($user);
+
+            return $user->makeHidden("id");
+
+        } catch (\Throwable $e) {
+
+            throw $e;
+
+        }
+    }
 }
