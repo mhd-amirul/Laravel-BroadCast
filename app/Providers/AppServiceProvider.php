66b7279a61c60db01f8412aeb3a6e72e5ea53eb1
mhd-amirul <amirulm236@gmail.com>
2023-02-15 09:39:15 +0700

    feat: configure file [response json, seed, appservice]

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Services\api_auth as ServicesApi_auth;
+use App\Services\Interfaces\api_auth;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(api_auth::class, ServicesApi_auth::class);
     }
 
     /**
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,13 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Chat;
+use App\Models\Message;
+use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,5 +26,62 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        User::create([
+            "name" => "amirul",
+            "email" => "amirul@mail.com",
+            "email_verified_at" => Carbon::now(),
+            "password" => Hash::make("password"),
+        ]);
+
+        User::create([
+            "name" => "muhammad",
+            "email" => "muhammad@mail.com",
+            "email_verified_at" => Carbon::now(),
+            "password" => Hash::make("password"),
+        ]);
+
+        User::create([
+            "name" => "Syeikh",
+            "email" => "Syeikh@mail.com",
+            "email_verified_at" => Carbon::now(),
+            "password" => Hash::make("password"),
+        ]);
+
+        Chat::create([
+            "user_id" => "amirul@mail.com_|_muhammad@mail.com",
+        ]);
+
+        Chat::create([
+            "user_id" => "amirul@mail.com_|_Syeikh@mail.com",
+        ]);
+
+        Message::create([
+            "message" => "hi",
+            "chat_id" => 1,
+            "sender_id" => "amirul@mail.com",
+            "to_id" => "muhammad@mail.com",
+        ]);
+
+        Message::create([
+            "message" => "hi too",
+            "chat_id" => 1,
+            "sender_id" => "muhammad@mail.com",
+            "to_id" => "amirul@mail.com",
+        ]);
+
+        Message::create([
+            "message" => "hi",
+            "chat_id" => 2,
+            "sender_id" => "amirul@mail.com",
+            "to_id" => "Syeikh@mail.com",
+        ]);
+
+        Message::create([
+            "message" => "hi too",
+            "chat_id" => 2,
+            "sender_id" => "Syeikh@mail.com",
+            "to_id" => "amirul@mail.com",
+        ]);
     }
 }
