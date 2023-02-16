6194cedc1626d7bc933c19deb05b735ab8531a88
mhd-amirul <amirulm236@gmail.com>
2023-02-16 18:13:15 +0700

    fix: refactoring sign in method

--- app/Services/api_auth.php
+++ app/Services/api_auth.php
@@ -24,4 +24,22 @@ class api_auth implements InterfacesApi_auth {
 
         }
     }
+
+    public function sign_in(array $user)
+    {
+        $user_db = User::where("email", $user["email"])->first();
+
+        if ($user_db) {
+            if (Hash::check($user["password"], $user_db->password)) {
+
+                $token = $user_db->createToken("token")->plainTextToken;
+
+                return ["message" => "sign in success!", "token" => $token, "code" => 200];
+            }
+
+            return ["message" => "email or password not compatible!", "code" => 400];
+        }
+
+        return ["message" => "user not found!", "code" => 404];
+    }
 }
