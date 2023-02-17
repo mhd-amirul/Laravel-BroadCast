9e2bd29ba85639b2cd8ba176390da590ee5d42e5
mhd-amirul <amirulm236@gmail.com>
2023-02-17 14:16:04 +0700

    fix: load message using new structure database

--- app/Http/Controllers/AppMessageController.php
+++ app/Http/Controllers/AppMessageController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Events\AppSendMessagesEvent;
 use App\Models\Chat;
+use App\Models\Message;
 use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class AppMessageController extends Controller
@@ -19,12 +21,11 @@ class AppMessageController extends Controller
     public function load_message(Request $request)
     {
 
-        $user_id = $request->ids;
+        $chat = Chat::where("user_id", auth()->user()->email."_|_".$request->friendChat)->first();
 
-        $chat = Chat::whereJsonContains("user_id->member", $user_id)->first();
+        $user_id = explode("_|_", $chat->user_id);
 
-        $messages = $chat->messages
-        ->map( function ($msg) use ($user_id) {
+        $messages = $chat->messages->map( function ($msg) use ($user_id) {
             $messages = [];
             $msg->sender_id == $user_id[0] && $msg->to_id == $user_id[1] ? $messages = $msg : null;
             $msg->sender_id == $user_id[1] && $msg->to_id == $user_id[0] ? $messages = $msg : null;
@@ -33,29 +34,10 @@ class AppMessageController extends Controller
         ->filter(function ($msg) { return !empty($msg); })
         ->sort(function ($a, $b) { return $a["created_at"] <=> $b["created_at"]; });
 
-        return response()->json($messages);
+        return response()->json(array_values($messages->toArray()));
     }
 
-    public function created_message(Request $request)
-    {
-        // $user = Auth()->user();
 
-        $to = User::where("name", $request->nameOrEmail)
-            ->orWhere("email", $request->nameOrEmail)
-            ->select("id")
-            ->first();
-
-        $chat = Chat::whereJsonContains("user_id", json_encode(["amirul@mail.com"]  ))->first();
-
-        $msg = [
-            "chat_id" => 1,
-            // "sender_id" => $user->id,
-            "to_id" => $to
-        ];
-
-
-        return response()->json($chat);
-    }
 
     public function get_message()
     {
