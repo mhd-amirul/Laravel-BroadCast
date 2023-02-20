d4fde2cdc1786103ab8214a30ee0a9dbd3f706e0
mhd-amirul <amirulm236@gmail.com>
2023-02-20 10:21:56 +0700

    fix: refactoring get recent chat function

--- app/Http/Controllers/AppChatController.php
+++ app/Http/Controllers/AppChatController.php
@@ -2,12 +2,20 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Chat;
-use App\Models\Message;
+use App\Helpers\ResponseFormatter;
+use App\Services\Interfaces\api_chat;
 use Illuminate\Http\Request;
 
 class AppChatController extends Controller
 {
+
+    protected $api_chat;
+
+    public function __construct(api_chat $api_chat)
+    {
+        $this->api_chat = $api_chat;
+    }
+
     public function find_users()
     {
         # code...
@@ -15,17 +23,7 @@ class AppChatController extends Controller
 
     public function get_recent_chat()
     {
-        // get chat
-        $chat = Chat::where("user_id", "LIKE", "%" . auth()->user()->email . "%")->orderBy("updated_at", "desc")->get();
-
-        // get last message
-        $chat->map(function ($chat) {
-            $msg = $chat->messages->sort(function ($a, $b) { return $b["created_at"] <=> $a["created_at"]; })->take(1);
-            $chat->message = array_values($msg->toArray());
-            unset($chat->messages);
-        });
-
-        return response()->json($chat);
+        return ResponseFormatter::success($this->api_chat->get_recent_chat(auth()->user()->email));
     }
 
     public function search_recent_chat(Request $request)
