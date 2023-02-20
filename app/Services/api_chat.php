7f238732accd405ce087a28c968059028a64b359
mhd-amirul <amirulm236@gmail.com>
2023-02-20 10:21:49 +0700

    fix: refactoring get recent chat function

--- app/Services/api_chat.php
+++ app/Services/api_chat.php
@@ -2,8 +2,28 @@
 
 namespace App\Services;
 
+use App\Models\Chat;
 use App\Services\Interfaces\api_chat as InterfacesApi_chat;
 
 class api_chat implements InterfacesApi_chat {
 
+    public function get_chat($email, $ord = "ASC")
+    {
+        return Chat::where("user_id", "LIKE", "%" . $email . "%")->orderBy("updated_at", $ord)->get();
+    }
+
+    public function get_recent_chat($email)
+    {
+        // get chat
+        $chat = $this->get_chat($email, "DESC");
+
+        // get last message
+        $chat->map(function ($chat) {
+            $msg = $chat->messages->sort(function ($a, $b) { return $b["created_at"] <=> $a["created_at"]; })->take(1);
+            $chat->message = array_values($msg->toArray());
+            unset($chat->messages);
+        });
+
+        return $chat;
+    }
 }
