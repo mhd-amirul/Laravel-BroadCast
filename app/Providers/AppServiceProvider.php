b0d5c7aaa8472ff28ea89177d969f94987699f97
mhd-amirul <amirulm236@gmail.com>
2023-03-03 09:37:02 +0700

    fix: connect api_message interface with service

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,8 +4,10 @@ namespace App\Providers;
 
 use App\Services\api_auth as ServicesApi_auth;
 use App\Services\api_chat as ServicesApi_chat;
+use App\Services\api_message as ServicesApi_message;
 use App\Services\Interfaces\api_auth;
 use App\Services\Interfaces\api_chat;
+use App\Services\Interfaces\api_message;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,7 @@ class AppServiceProvider extends ServiceProvider
     {
         $this->app->bind(api_auth::class, ServicesApi_auth::class);
         $this->app->bind(api_chat::class, ServicesApi_chat::class);
+        $this->app->bind(api_message::class, ServicesApi_message::class);
     }
 
     /**
