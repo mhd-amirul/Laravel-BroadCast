601befdb156bf079305675f9fe310e83c3b0217f
mhd-amirul <amirulm236@gmail.com>
2023-03-03 09:15:24 +0700

    fix: connect api_chat interface with service

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,9 @@
 namespace App\Providers;
 
 use App\Services\api_auth as ServicesApi_auth;
+use App\Services\api_chat as ServicesApi_chat;
 use App\Services\Interfaces\api_auth;
+use App\Services\Interfaces\api_chat;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -16,6 +18,7 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         $this->app->bind(api_auth::class, ServicesApi_auth::class);
+        $this->app->bind(api_chat::class, ServicesApi_chat::class);
     }
 
     /**
