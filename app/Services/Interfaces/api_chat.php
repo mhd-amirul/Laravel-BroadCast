f0d480ffe839bb108e4bfce737caa28b6d0c48c6
mhd-amirul <amirulm236@gmail.com>
2023-03-03 09:29:18 +0700

    fix: refactoring get_chat_user_auth_with_partner function

--- app/Services/Interfaces/api_chat.php
+++ app/Services/Interfaces/api_chat.php
@@ -2,7 +2,11 @@
 
 namespace App\Services\Interfaces;
 
+use Illuminate\Database\Eloquent\Collection;
+
 interface api_chat {
     public function get_recent_chat($email);
     public function get_chat_user_auth($email, $ord = "ASC");
+    public function get_chat_user_auth_with_partner($friendChat);
+    // public function load_last_messages(Collection $chat);
 }
